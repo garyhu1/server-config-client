86af9511b60fce7633ba124839998d58026858aa
huweiwei <huweiwei@superengine.com.cn>
2018-11-19 18:29:53 +0800

    ok

---
+++ src/main/java/com/garyhu/ConfigClientApplication.java
@@ -0,0 +1,13 @@
+package com.garyhu;
+
+/**
+ * @author: garyhu
+ * @since: 2018/11/19 0019
+ * @decription:
+ */
+public class ConfigClientApplication {
+
+    public static void main(String[] args)throws Exception{
+
+    }
+}
