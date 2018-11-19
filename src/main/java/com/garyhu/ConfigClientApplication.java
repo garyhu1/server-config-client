720dca67ea0599fb384ca8a57b339486f212811e
huweiwei <huweiwei@superengine.com.cn>
2018-11-19 18:30:37 +0800

    ok

--- src/main/java/com/garyhu/ConfigClientApplication.java
+++ src/main/java/com/garyhu/ConfigClientApplication.java
@@ -1,13 +1,17 @@
 package com.garyhu;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author: garyhu
  * @since: 2018/11/19 0019
  * @decription:
  */
+@SpringBootApplication
 public class ConfigClientApplication {
 
     public static void main(String[] args)throws Exception{
-
+        SpringApplication.run(ConfigClientApplication.class,args);
     }
 }
